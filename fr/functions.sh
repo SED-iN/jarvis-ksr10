68c578eb3518fc52886722e84e11bea19ed7bbc8
Sam Seban <s.seban@sed-in.com>
2018-01-22 22:18:36 +0100

    Update functions.sh

--- fr/functions.sh
+++ fr/functions.sh
@@ -2,7 +2,8 @@
 
 jv_pg_waveHello(){
 
-os.system('./commander.py commands.csv')
+#python /BRASROBOT/maplinarm/commander.py commands.csv
+os.system('/BRASROBOT/maplinarm/commander.py commands.csv')
 
 done
 }
