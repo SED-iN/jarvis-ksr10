4ac7d0de0176b7288028d19a1f808b7615731665
Sam Seban <s.seban@sed-in.com>
2018-01-29 12:43:41 +0100

    Update functions.sh

--- fr/functions.sh
+++ fr/functions.sh
@@ -7,7 +7,7 @@ cmd2 = './commander.py commands.csv'
 cmd1 = 'cd BRASROBOT/maplinarm'
 os.system(cmd1)
 os.system(cmd2)
-python /BRASROBOT/maplinarm/commander.py commands.csv;
+python /BRASROBOT/maplinarm/commander.py commands.csv
 #os.system('BRASROBOT/maplinarm/commander.py commands.csv')
 
 done
