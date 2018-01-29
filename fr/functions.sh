6cc4c5ddbab0c52ae098c283c6521e00be537d94
Sam Seban <s.seban@sed-in.com>
2018-01-29 12:48:00 +0100

    Update functions.sh

--- fr/functions.sh
+++ fr/functions.sh
@@ -1,13 +1,16 @@
-#!/bin/sh
+#!/usr/bin/env python
 
 jv_pg_waveHello(){
 
-import os
-cmd2 = './commander.py commands.csv'
-cmd1 = 'cd BRASROBOT/maplinarm'
-os.system(cmd1)
-os.system(cmd2)
-python /BRASROBOT/maplinarm/commander.py commands.csv
+cd BRASROBOT/maplinarm
+python ./commander.py commands.csv 
+#import os
+#cmd2 = './commander.py commands.csv'
+#cmd1 = 'cd BRASROBOT/maplinarm'
+#os.system(cmd1)
+#os.system(cmd2)
+#python /BRASROBOT/maplinarm/commander.py commands.csv
+
 #os.system('BRASROBOT/maplinarm/commander.py commands.csv')
 
 done
